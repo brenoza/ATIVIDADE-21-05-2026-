8d24b2b45c7ad0aa3493fe4305b45666a86fe5ea
brenoza <brenojustino669@gmail.com>
2026-05-21 08:31:49 -0300

    Add files via upload

---
+++ ATIVIDADE (21-05-2026)/TEMPLETES/app.py
@@ -0,0 +1,26 @@
+from flask import flask, render_templetes
+from flask import request
+
+app = flask(__name__)
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_templetes('login.html')
+
+@app.route('/autenticador', methods = ['GET'])
+def autenticar():
+    usuario = request.args.get('usuario')
+    senha = request.args.get('senha')
+    DATA = request.args.get('DATA')
+    LOCALIDADE = request.args.get('LOCALIDADE')
+    return render_templetes(
+        'autenticador.html',
+        usuario=usuario,
+        senha=senha,
+        DATA=DATA,
+        LOCALIDADE=LOCALIDADE
+    )
+
+if __name__ == '__main__':
+    app.run(debug=True)
